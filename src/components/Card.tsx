349b865111efe78bee85e18dd552f7a39efa763d
JoeG67 <joachimgerard66@gmail.com>
2025-08-11 14:31:58 +0800

    Updated background, font colors and paddings for better spacing to avoid types breaking into a second line

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -15,12 +15,12 @@ const Card: React.FC<CardProps> = ({ url, onOpenPopup }) => {
 
   const typeColors: { [key: string]: string } = {
     normal: "bg-[#9FA19F] text-white",
-    fire: "bg-[#E62823] text-white",
-    water: "bg-[#2980EF] text-white",
+    fire: "bg-[#E03A3A] text-white",
+    water: "bg-[#1e90ff] text-white",
     electric: "bg-[#FAC000] text-white",
-    grass: "bg-[#588d2a] text-white",
+    grass: "bg-[#50C878] text-white",
     ice: "bg-[#3DCEF3] text-white",
-    fighting: "bg-[#FF8000] text-white",
+    fighting: "bg-[#bf5858] text-white",
     poison: "bg-[#9141CB] text-white",
     ground: "bg-[#915121] text-white",
     flying: "bg-[#81B9EF] text-white",
@@ -28,7 +28,7 @@ const Card: React.FC<CardProps> = ({ url, onOpenPopup }) => {
     bug: "bg-[#91A119] text-white",
     rock: "bg-[#AFA981] text-white",
     ghost: "bg-[#704170] text-white",
-    dragon: "bg-[#5060E1] text-white",
+    dragon: "bg-[#882eff] text-white",
     dark: "bg-[#624D4E] text-white",
     steel: "bg-[#60A1B8] text-white",
     fairy: "bg-[#EF70EF] text-white",
@@ -75,11 +75,11 @@ const Card: React.FC<CardProps> = ({ url, onOpenPopup }) => {
         className="card bg-white rounded-lg overflow-hidden shadow-md hover:shadow-xl border border-black"
         onClick={handleCardClick}
       >
-        <div className="pokemon-image relative">
+        <div className="pokemon-image bg-blue-100 relative">
           <img
             src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
             alt={pokemon.name}
-            className="mx-auto w-8 sm:w-12 md:w-24 lg:w-32 xl:w-48"
+            className="mx-auto w-8 sm:w-12 md:w-24 lg:w-32 xl:w-44"
           />
         </div>
         <div className={`p-4 ${getCardBackground()} border-t  border-black flex flex-col items-center text-center`}>
@@ -94,7 +94,7 @@ const Card: React.FC<CardProps> = ({ url, onOpenPopup }) => {
                 className={`type-box ${
                   typeColors[type.name.toLowerCase()] ||
                   "bg-gray-300 text-gray-700"
-                } px-2 py-1 rounded text-xs sm:text-sm md:text-base lg:text-lg font-bold font-pokemon tracking-widest border-2 border-black`}
+                } px-1 py-1 rounded text-[10px] sm:text-xs md:text-sm lg:text-base font-bold font-pokemon tracking-widest border-2 border-black`}
               >
                 {type.name}
               </div>
