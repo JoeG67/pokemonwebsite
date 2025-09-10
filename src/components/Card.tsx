f0f8d8c7078e0ecbb9cf7483aa3687aeb4402af8
JoeG67 <joachimgerard66@gmail.com>
2025-09-10 15:07:53 +0800

    Removed irrelevant comments, adjusted formatting

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Pokemon } from "./Pokemon"; // Import Pokemon interface
+import { Pokemon } from "./Pokemon";
+import { typeColors } from "../constants/typeColors";
 
 interface CardProps {
   url: string;
@@ -12,27 +13,6 @@ const Card: React.FC<CardProps> = ({ url, onOpenPopup }) => {
     id: 0,
     types: [],
   });
-
-  const typeColors: { [key: string]: string } = {
-    normal: "bg-[#9FA19F] text-white",
-    fire: "bg-[#E03A3A] text-white",
-    water: "bg-[#1e90ff] text-white",
-    electric: "bg-[#FAC000] text-white",
-    grass: "bg-[#50C878] text-white",
-    ice: "bg-[#3DCEF3] text-white",
-    fighting: "bg-[#bf5858] text-white",
-    poison: "bg-[#9141CB] text-white",
-    ground: "bg-[#915121] text-white",
-    flying: "bg-[#81B9EF] text-white",
-    psychic: "bg-[#EF4179] text-white",
-    bug: "bg-[#91A119] text-white",
-    rock: "bg-[#AFA981] text-white",
-    ghost: "bg-[#704170] text-white",
-    dragon: "bg-[#882eff] text-white",
-    dark: "bg-[#624D4E] text-white",
-    steel: "bg-[#60A1B8] text-white",
-    fairy: "bg-[#EF70EF] text-white",
-  };
 
   useEffect(() => {
     const pokemonIndex = url.split("/")[url.split("/").length - 2];
@@ -56,18 +36,17 @@ const Card: React.FC<CardProps> = ({ url, onOpenPopup }) => {
     onOpenPopup(pokemon);
   };
 
-    const getCardBackground = () => {
-      if (pokemon.types.length === 1) {
-        return typeColors[pokemon.types[0].name.toLowerCase()] || "bg-gray-300";
-      }
-    
-      if (pokemon.types.length === 2) {
-        return typeColors[pokemon.types[0].name.toLowerCase()] || "bg-gray-300";
-      }
-    
-      return "bg-red-300"; // Default background for other cases
-    };
+  const getCardBackground = () => {
+    if (pokemon.types.length === 1) {
+      return typeColors[pokemon.types[0].name.toLowerCase()] || "bg-gray-300";
+    }
 
+    if (pokemon.types.length === 2) {
+      return typeColors[pokemon.types[0].name.toLowerCase()] || "bg-gray-300";
+    }
+
+    return "bg-red-300";
+  };
 
   return (
     <section>
@@ -82,8 +61,9 @@ const Card: React.FC<CardProps> = ({ url, onOpenPopup }) => {
             className="mx-auto w-8 sm:w-12 md:w-24 lg:w-32 xl:w-44"
           />
         </div>
-        <div className={`p-4 ${getCardBackground()} border-t  border-black flex flex-col items-center text-center`}>
-        {/* <p className="text-black font-bold text-sm sm:text-base md:text-lg lg:text-xl mb-2">NO: {pokemon.id}</p> */}
+        <div
+          className={`p-4 ${getCardBackground()} border-t  border-black flex flex-col items-center text-center`}
+        >
           <h3 className="text-base text-black font-bold font-pokemon tracking-widest sm:text-lg md:text-xl lg:text-2xl mb-2">
             {pokemon.name}
           </h3>
