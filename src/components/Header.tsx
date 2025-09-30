4b73730a052cb524d7a8f4b402b144d922a31968
JoeG67 <joachimgerard66@gmail.com>
2025-09-30 11:09:20 +0800

    Added in hover effects for logo, github link and search bar + input

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,31 +23,30 @@ const Header: React.FC<HeaderProps> = ({
           <img
             src="https://www.pngarts.com/files/4/Pokeball-PNG-Image-Transparent-Background.png"
             alt="Page Title"
-            className="h-12 w-12"
+            className="h-12 w-12 transition-transform duration-300 ease-in-out hover:scale-[1.1]"
           />
         </div>
 
-<div className="flex-1 flex justify-center items-center gap-x-4">
-  <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-  <input
-    type="number"
-    placeholder="Limit"
-    value={limit}
-    onChange={(e) => {
-      const value = Number(e.target.value);
-      setLimit(isNaN(value) || value < 0 ? 0 : value);
-    }}
-    className="border border-black rounded px-3 py-1 w-20 text-center font-pokemon bg-white shadow-sm"
-  />
-</div>
-
-
+        <div className="flex-1 flex justify-center items-center gap-x-4">
+          <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          <input
+            type="number"
+            placeholder="Limit"
+            value={limit}
+            onChange={(e) => {
+              const value = Number(e.target.value);
+              setLimit(isNaN(value) || value < 0 ? 0 : value);
+            }}
+            className="border border-black rounded-full px-3 py-1 w-20 text-center font-pokemon bg-white shadow-sm
+            transition-transform duration-300 ease-in-out hover:scale-[1.1]"
+          />
+        </div>
         <div className="flex items-center px-10">
           <a href={githubUrl} target="_blank" rel="noopener noreferrer">
             <img
               src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
               alt="GitHub Link"
-              className="h-12 w-12"
+              className="h-12 w-12 transition-transform duration-300 ease-in-out hover:scale-[1.1]"
             />
           </a>
         </div>
